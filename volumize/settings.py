78c271ac474ab74e6442be3c08b858ea20c76771
Zhassulan1 <zhkainazarov@gmail.com>
2024-07-09 13:18:49 +0500

    fix: fixed error with debug mode being on even if .env set to False

--- volumize/settings.py
+++ volumize/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG_MODE"))
+DEBUG = (os.environ.get("DEBUG_MODE") == "True")
 
 if DEBUG == True:
     print("DEBUG MODE IS ON")
