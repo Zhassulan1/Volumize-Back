601c25d19cca5472053303152dac708a534ce44e
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-07-28 00:48:58 +0000

    fix

--- volumize/settings.py
+++ volumize/settings.py
@@ -124,6 +124,10 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = ["https://volumize-ai.vercel.app"]
 
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'https://*',
+]
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
