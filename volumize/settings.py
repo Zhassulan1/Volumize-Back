ac1b6c74726d62bcab7dd71f3f7b41a7e1d1ca8e
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-08-01 09:46:32 +0000

    fix: restricted allowed origins"

--- volumize/settings.py
+++ volumize/settings.py
@@ -120,13 +120,19 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ["https://volumize-ai.vercel.app", "https://volumize.me"]
+CORS_ALLOWED_ORIGINS = [
+    "https://volumize-ai.vercel.app", 
+    "https://volumize.me",
+    "https://www.volumize-ai.vercel.app",
+    "https://www.volumize.me"
+]
+
 if DEBUG:
     CORS_ALLOWED_ORIGINS.append("http://localhost:3000")
 
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_CREDENTIALS = True
 
 
 # Internationalization
